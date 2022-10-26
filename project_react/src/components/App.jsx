6a326212791f66d4f68c24d0297460f41f0c2d06
Jared Ephemeral <jared.menrod@gmail.com>
2022-10-25 19:53:07 -0600

    ok

--- project_react/src/components/App.jsx
+++ project_react/src/components/App.jsx
@@ -1,11 +1,18 @@
-import { useState } from 'react'
+import { useEffect, useState } from 'react'
 import '../styles/App.css'
 
 function App () {
-  const [username, setUserName] = useState ('Bruno')
+  const [username, setUserName] = useState('Bruno')
+  const handleChangeName = () => setUserName('Jared')
+
+  useEffect(() => {
+    console.log('First')
+  }, [])
+
   return (
     <div className='App'>
       <h2>{username}</h2>
+      <button onClick={handleChangeName}>Cambiar</button>
     </div>
   )
 }
--- project_react/src/main.jsx
+++ project_react/src/main.jsx
@@ -1,4 +1,3 @@
-import { StrictMode } from 'react'
 import { createRoot } from 'react-dom/client'
 
 import 'bootstrap/dist/css/bootstrap.min.css'
@@ -8,8 +7,4 @@ import App from './components/App'
 
 const root = document.getElementById('root')
 
-createRoot(root).render(
-  <StrictMode>
-    <App />
-  </StrictMode>
-)
+createRoot(root).render(<App />)
